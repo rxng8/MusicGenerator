d157eaee2881f1387e7e44632b2e6e55b18e59ac
dewanderex <nguyvi01@gettysburg.edu>
2019-09-28 22:23:30 -0400

    More on Data Preprocessing and generator.

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ import librosa
 from pydub import AudioSegment
 
 # Midi Processing library
-from mido import MidiFile
-from mido import Message, MetaMessage
+from mido import MidiFile, MidiTrack, Message, MetaMessage
 from mido import tick2second, second2tick
 
 # Math Library
@@ -40,34 +39,76 @@ from keras.metrics import categorical_accuracy
 from keras.utils import Sequence
 from keras.optimizers import Adam, SGD, RMSprop
 
+# Utils
+import utils
+
+
 # %%
 #------------------------------- CONSTANTS ----------------------------------------#
 
-atavachron = 'test_data/Atavachron.mid'
-
+ata = 'test_data/Atavachron.mid'
 noc = 'test_data/chno0902.mid'
+noo = 'test_data/NoOneInTheWorld.mid'
 
-midi = MidiFile(noc)
+tpb = midi.ticks_per_beat
+
+DATA_FOLDER_PATH = 'dataset_piano_jazz'
+MIDI_NOTES = np.arange(21, 109)
+MIDI_NOTES_MAP = {
+    '21': 'A0',
+    '22': 'B0',
+    # TODO: Implement!
+}
+MIDI_PITCH_TO_INDEX_CONSTANT = np.min(MIDI_NOTES)
+TICK_SCALER = 0.1
+
+testFile = 'dataset/2004/MIDI-Unprocessed_SMF_02_R1_2004_01-05_ORIG_MID--AUDIO_02_R1_2004_05_Track05_wav.midi'
+midi = MidiFile(noo)
+
 
 # %%
 
-#Test note off
-x = midi.tracks[1]
-i = 0
-for msg in x:
-    print(msg)
-    if i >= 200:
-        break
-    i += 1
+midi
 # %%
-for msg in midi.play():
+
+for fname in os.listdir(DATA_FOLDER_PATH):
+    midi = MidiFile(os.path.join(DATA_FOLDER_PATH, fname))
+    print(midi)
+
+# %%
+
+midi.tracks[1][6]
+
+
+# %%
+len(MIDI_NOTES)
+
+# %%
+
+midi.tracks
+
+findNoteDuration(69, midi.tracks[0][3:])
+# %%
+
+def print_trackk(track):
+    '''
+    Do Something
+
+
+    '''
+    for i, msg in enumerate(track):
+        print(msg)
+    return
+
+# %%
+
+for i, msg in enumerate(a.tracks[1]):
+    
     print(msg)
-
-
 # %%
 
 tpb = midi.ticks_per_beat
-tempo = midi.tracks[0][2].tempo
+# tempo = midi.tracks[0][2].tempo
 # time = midi.tracks[1][2].time
 
 # %%
@@ -88,8 +129,19 @@ for j, message in enumerate(midi.tracks[1]):
 
 # %%
 
-notes
 
+x = [0, 1, np.nan, 1, 2,np.nan, 1, 3]
+y = [56, 56, np.nan, 54, 54,np.nan, 45, 45]
+
+plt.plot(x, y)
+
+# %%
+
+nann = np.empty((2,10))
+nann[:] = np.nan
+# %%
+
+nann
 # %%
 max_line = 200
 
@@ -107,10 +159,213 @@ plt.show()
 # %%
 plt.figure(figsize=(30,8))
 plt.scatter(times[:100], notes[:100])
+# %%
+# Constants for instrument creation
+
+#------------------------------------------- Note Class ---------------------------------------------------#
+
+PERC = True
+INST = False
+class MEvent:
+    """
+    MEvent is a fairly direct representation of Haskell Euterpea's MEvent type,
+    which is for event-style reasoning much like a piano roll representation.
+    eTime is absolute time for a tempo of 120bpm. So, 0.25 is a quarter note at
+    128bpm. The patch field should be a patch number, like the patch field of
+    the Instrument class.
+    """
+    def __init__(self, eTime, pitch, duration, velocity=64, patch=(-1, INST)):
+        self.eTime = eTime  # current time
+        self.pitch = pitch
+        self.duration = duration
+        self.velocity = velocity
+        self.patch = patch
+        self.sTime = eTime + self.duration  # Stop time
+
+    def __str__(self):
+        return "MEvent(eTime: {0}, pitch: {1}, duration: {2}, velocity: {3}, patch: {4}, sTime: {5})".format(str(self.eTime), str(self.pitch), str(self.duration), str(self.velocity), str(self.patch), str(self.sTime))
+
+    def __repr__(self):
+        return str(self)
 
 # %%
 
-class DataPreprocessor:
+print_trackk(midi.tracks[0])
+# %%
+
+#------------------------------ Data Preprocessing ---------------------------------#
+
+def findNoteDuration(pitch, channel, events):
+    '''
+    Scan through a list of MIDI events looking for a matching note-off.
+    A note-on of the same pitch will also count to end the current note,
+    assuming an instrument can't play the same note twice simultaneously.
+    If no note-off is found, the end of the track is used to truncate
+    the current note.
+
+    Adding one more case: Channel is mixed within tracks
+    :param pitch:
+    :param events:
+    :return:
+    '''
+    sumTicks = 0
+    for e in events:
+        #sumTicks = sumTicks + e.tick
+        sumTicks = sumTicks + e.time
+        #c = e.__class__.__name__
+        c = e.type
+        #if c == "NoteOffEvent" or c == "NoteOnEvent":
+        if e.channel == channel and (c == "note_on" or c == "note_off"):
+            if e.note == pitch:
+                return sumTicks
+    return sumTicks
+
+
+def tickToDur(ticks, resolution = 96):
+    '''
+    Convert from pythonmidi ticks back to PythonEuterpea durations
+    :param ticks: number of ticks
+    :param resolution: ticks_per_beat
+    :return:
+    '''
+    #return float(ticks) / float((RESOLUTION * 4))
+    return round(float(ticks) * TICK_SCALER)
+    # return ticks // (resolution * 4)
+
+def getChannel(track):
+    '''
+    Determine the channel assigned to a track.
+    ASSUMPTION: all events in the track should have the same channel.
+    :param track:
+    :return:
+    '''
+    if len(track) > 0:
+        e = track[0]
+        #if (e.__class__.__name__ == "EndOfTrackEvent"): # mido has no end of track?
+        #    return -1
+        if track[0].type == 'note_on' or track[0].type=='note_off':
+            return track[0].channel
+    return -1
+
+def structurize_track(midi_track, ticks_per_beat, default_patch=-1):
+    '''
+    
+    '''
+
+    currChannel = -1
+    currTick = 0
+    currPatch = default_patch
+
+    max_stop_tick = -np.Infinity
+
+    stred = []
+
+    for i, msg in enumerate(midi_track):
+        _type = msg.type
+
+        if isinstance(msg, MetaMessage):
+            continue
+
+        currChannel = msg.channel
+        currTick += msg.time
+
+        if _type == 'program_change':
+            currPatch = msg.program
+        elif _type == 'control_change':
+            pass
+        elif _type == 'note_on':
+            # Finding durtation of the note!
+            tick_duration = findNoteDuration(msg.note, currChannel, midi_track[(i+1):])
+            # Create instance for this note!
+            # event = MEvent(tickToDur(currTick, ticks_per_beat), msg.note, tickToDur(tick_duration, ticks_per_beat), msg.velocity, currPatch)
+            event = MEvent(tickToDur(currTick), msg.note, tickToDur(tick_duration), msg.velocity, currPatch)
+            # stred.append([currChannel, event]) # Does Current channel matter in the whole picture?
+            stred.append(event)
+
+        elif _type == 'time_signature':
+            print("TO-DO: handle time signature event")
+            pass
+        elif _type == 'key_signature':
+            print("TO-DO: handle key signature event")
+            pass # need to handle this later
+        elif _type == 'note_off' or 'end_of_track':
+            pass # nothing to do here; note offs and track ends are handled in on-off matching in other cases.
+        else:
+            print("Unsupported event type (ignored): ", e.type, vars(e),e)
+            pass
+    return stred, tickToDur(currTick)
+
+def map_note_to_graph(stred):
+    '''
+    Take a structured array of notes and map to graph
+    :param stred:
+    :return x:
+    :return y:
+    '''
+    x = []
+    y = []
+    for i, e in enumerate(stred):
+        x = np.append(x, [e.pitch, e.pitch, np.nan])
+        y = np.append(y, [e.eTime, e.eTime + e.duration, np.nan])
+    return x, y
+
+def map_note_to_array(stred, max_tick):
+    '''
+    Create a 2D array out of a structures file!
+    :param stred: array of MEvent
+    :return array: a 2D array with shape (max_tick, note range) where x axis is pitch and y axis is time.
+    '''
+    array = np.zeros(shape=(max_tick, MIDI_NOTES.shape[0]))
+
+    for i, event in enumerate(stred):
+        eTime = event.eTime
+        sTime = event.sTime
+        pitch = event.pitch - MIDI_PITCH_TO_INDEX_CONSTANT
+        velocity = event.velocity
+        array[eTime:sTime, pitch] += velocity
+
+    return array
+
+
+# %%
+
+midi = MidiFile(noo)
+tpb = midi.ticks_per_beat
+st, maxTick = structurize_track(midi.tracks[0], tpb)
+
+# x, y = map_note_to_graph(structurize_track(midi.tracks[0], tpb))
+arr = map_note_to_array(st, maxTick)
+
+# %%
+# plt.figure(figsize=(14,6))
+
+plt.imshow(arr[2800:3000,:])
+
+# %%
+
+arr
+# %%
+cnt = 0
+for e in st:
+    print(e)
+    # if cnt == 100:
+    #     break
+    cnt += 1
+
+# %%
+
+cnt = 0
+for e in midi.tracks[0]:
+    print(e)
+    if cnt == 100:
+        break
+    cnt += 1
+
+
+# %%
+plt.figure(figsize=(20,6))
+plt.plot(y[:], x[:])
+
 
 # %%
 
@@ -139,11 +394,9 @@ class DataGenerator(Sequence):
         :var self.tokenizer:
         :var self.data_path:
         """
-        self.classes = []
-        self.labels = []
         self.fname = []
+
         self.data = []
-        self.data_validation = []
 
         self.validation_split = validation_split
         self.data_size = 0
@@ -162,13 +415,77 @@ class DataGenerator(Sequence):
     :param data_path: (String) The actual base folder of data
     """
     def load_data(self):
+        cnt = 0
+        for i, _file in enumerate(os.listdir(self.data_path)):
+            fname = os.path.join(self.data_path, _file)
+            # TODO: Check if the data is good
+            
+            midi = MidiFile(fname)
+            print('{} has {} tracks'.format(fname, len(midi.tracks)))
 
-        
+            self.fname.append(fname)
+            cnt +=1
+
+        print('Found {} midi file in data folder!'.format(cnt))
+
     """
     Utilities method for classes
     :param filename: Name of the file
     """
     def load_midifile(filename):
+        return
+
+    # Temp extract data
+    def extract_data(self):
+        for fname in self.fname:
+            midi = MidiFile(fname)
+            tpb = midi.ticks_per_beat
+            for i, track in enumerate(midi.tracks):
+                # x, y = map_note_to_graph(structurize_track(midi.tracks[0], tpb))
+                # arr = map_note_to_array(*structurize_track(track, tpb))
+                st, maxTick = structurize_track(track, tpb)
+                arr = map_note_to_array(st, maxTick)
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        return
+
+    def __len__(self):
+        return
+
+    def __getitem__(self, index):
+        return
+
+# %%
+
+
+dataGen = DataGenerator(DATA_FOLDER_PATH)
+
+
+# %%
+
+# Dummy Datagen
+
+class DummyGen(Sequence):
+    def __init__(self, batch_size=32, dim=(None, None),
+             n_classes=10, shuffle=True, validation_split=0.1):
+
+        self.data_shape = (None,None)
+        self.dim = dim
+        self.batch_size = batch_size
+        self.list_IDs = []
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+        self.load_data()
+        
+
+    def load_data(self):
 
 
     def on_epoch_end(self):
@@ -186,12 +503,166 @@ class DataGenerator(Sequence):
         return
 
 
+# %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # %%
 
-x.tracks[11][30]
+#---------------------------------------------------- MIDI Write file ----------------------------------#
+
+composed = MidiFile()
+composed.ticks_per_beat = 96
+
+
+track = MidiTrack()
+
+track.append(Message('note_on', note=64, velocity=64, time=0))
+track.append(Message('note_on', note=62, velocity=64, time=200))
+track.append(Message('note_on', note=60, velocity=64, time=200))
+track.append(Message('note_off', note=64, velocity=64, time=200))
+track.append(Message('note_off', note=62, velocity=64, time=200))
+track.append(Message('note_off', note=60, velocity=64, time=200))
+
+composed.tracks.append(track)
+
+composed.save('composed.mid')
+
+
 # %%
 
-isinstance(x.tracks[1][300], MetaMessage)
+compo = MidiFile('composed.mid')
+
+# %%
+
+compo.ticks_per_beat
 
 
+# %%
+#--------------------------------------------------Test--------------------------------------------------------#
+
+from keras.layers import LSTM, TimeDistributed
+# Dummy LSTM model
+
+def dummyModel():
+    in_tensor = Input (shape=(None, 5))
+    tensor = LSTM(128, activation='relu')(in_tensor)
+    tensor = Dense(1, activation='sigmoid')(tensor)
+    
+    model = Model(in_tensor, tensor)
+
+    model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# %%
+
+model = dummyModel()
+model.summary()
+# %%
+
+def train_generator():
+    while True:
+        sequence_length = np.random.randint(10, 100)
+        x_train = np.random.random((1000, sequence_length, 5))
+        # y_train will depend on past 5 timesteps of x
+        y_train = x_train[:,0,0]
+        y_train = to_categorical(y_train)
+        yield x_train, y_train
+# %%
+model.fit_generator(train_generator(), steps_per_epoch=30, epochs=10, verbose=1)
+
+# %%
+
+for x,y in train_generator():
+    print(x.shape)
+
+
+# %%
+from keras.layers import UpSampling2D
+def img_auto_encoder():
+    input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
+
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+    autoencoder = Model(input_img, decoded)
+    
+    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+    return autoencoder
+# %%
+
+model = img_auto_encoder()
+model.summary()
+
+# %%
+
+import numpy as np
+
+# %%
+
+count = 0
+for x in range(-20, 20, 1):
+    for y in range (-20, 20, 1):
+        if x * x + y * y <= 100:
+            print ('{}^2+{}^2={}'.format(x, y, x * x + y * y))
+            count += 1
+
+count
